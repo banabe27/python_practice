c33bd88cc757d0dcfa09921b8cc3b496e84e8021
banabe27 <banjiabegunde@gmail.com>
2025-08-26 17:23:13 +0100

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 football_players = [1, 3, 8, 9]
-player_info = {"cech":1, "carlos":3, "lampard":8, "tammy":9}
+player_info = {"cech": 1, "carlos": 3, "lampard": 8, "tammy": 9}
 print(player_info["carlos"])
-#gggg
