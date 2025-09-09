7b717dc985a1e8dadcd4c68e4faff38892022ceb
banabe27 <banjiabegunde@gmail.com>
2025-09-09 15:33:51 +0100

    practices

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
-# print(f"\multipli int(input(5))cation Table of {x}:\9")
-y = 23
-b = 23 
-if y != b:
-    print(f"{y} is not equal to {b}")
-elif y == b:
-    print(f"{y} is equal to {b}")
+# Student_score
+total_score = 100
+score = 42
+if (score) >= 50:
+    print("Pass")
 else:
-    print("default")
+    print("Fail")
+
+# Even_or_odd
+x = 5
+if (x) % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Weather_advice
+temperature_celcius = 25
+if (temperature_celcius) >= 30:
+    print("Hot, stay hydrated.")
+elif 20 <= (temperature_celcius) <= 29:
+    print("Nice weather.")
+else:
+    print("Cold, wear a jacket.")    
+
+#Election_results 
+candidiate_a_in_percentage = 48
+if (candidiate_a_in_percentage) >= 50:
+    print("Candidiate A wins")
+else:
+    print("Runoff required")    
+
+# Football_score
+home_team_goals = 4
+away_team_goals = 2
+if home_team_goals > away_team_goals:
+    print("Win")
+elif home_team_goals == away_team_goals:
+    print("Draw")
+else:
+    print("Lose")        
